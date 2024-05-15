972c04ad298a49ca296470e1b2bd159669d33335
Fabián Vargas <fabidev18@gmail.com>
2024-05-15 17:24:21 -0600

    Update GameScreen to lazy load houses and implement movement boundaries

--- src/screens/game_screen.py
+++ src/screens/game_screen.py
@@ -8,9 +8,14 @@ from characters.player import Player
 from screens.screen_manager import ScreenManager
 
 player_mock = {
-    "id": 1, 
-    "name": "John", 
-    "age": 25, 
+    "id": "5",
+    "name": "Cannon",
+    "gender": "Male",
+    "age": "27",
+    "marital_status": "Single",
+    "alive": "Alive",
+    "father": "0",
+    "mother": "1",
     "house": {"x": 250, "y": 250}
 }
 
@@ -23,8 +28,10 @@ class GameScreen(Screen):
         self.player = Player(player_mock, self.screen_manager.screen)
         self.screen_manager.screen.fill(Colors.GRASS.value)
 
-        houses_data = self.screen_manager.api.get_houses(self.player.get_pos())
-        self.houses = [ House(house_info, self.screen_manager.screen, self.player.get_pos()) for house_info in houses_data ]
+        self.houses = []
+        self.update_houses()
+        self.dx_counter = 0
+        self.dy_counter = 0
 
     def handle_events(self, events: list[pygame.event.Event]):
         """Handle pygame events.
@@ -54,26 +61,35 @@ class GameScreen(Screen):
         if horizontal_movement != 0 and vertical_movement != 0:
             self.player.move(horizontal_movement * 300 * self.screen_manager.dt, 0)
 
-            if self.player.get_pos()[0] > 0:
+            self.dx_counter += horizontal_movement * 300 * self.screen_manager.dt
+
+            if self.player.pos[0] > 0:
                 self.map.move(-horizontal_movement * 300 * self.screen_manager.dt, 0)
                 for house in self.houses:
                     house.move(-horizontal_movement * 300 * self.screen_manager.dt, 0)
             
         elif horizontal_movement != 0:
             self.player.move(horizontal_movement * 300 * self.screen_manager.dt, 0)
+            self.dx_counter += horizontal_movement * 300 * self.screen_manager.dt
 
-            if self.player.get_pos()[0] > 0:
+            if self.player.pos[0] > 0:
                 self.map.move(-horizontal_movement * 300 * self.screen_manager.dt, 0)
                 for house in self.houses:
                     house.move(-horizontal_movement * 300 * self.screen_manager.dt, 0)
 
         elif vertical_movement != 0:
             self.player.move(0, vertical_movement * 300 * self.screen_manager.dt)
+            self.dy_counter += vertical_movement * 300 * self.screen_manager.dt
             
-            if self.player.get_pos()[1] > 0:
+            if self.player.pos[1] > 0:
                 self.map.move(0, -vertical_movement * 300 * self.screen_manager.dt)
                 for house in self.houses:
                     house.move(0, -vertical_movement * 300 * self.screen_manager.dt)
+
+        if abs(self.dx_counter) >= self.screen_manager.screen.get_height() // 2 or abs(self.dy_counter) >= self.screen_manager.screen.get_height() // 2:
+            self.update_houses()
+            self.dx_counter = 0
+            self.dy_counter = 0
 
         if keys[pygame.K_ESCAPE]:
             pygame.quit()
@@ -89,10 +105,11 @@ class GameScreen(Screen):
         for house in self.houses:
             house.draw()
 
-        pos_text = pygame.font.Font(None, 30).render(f"Posición: {int(self.player.get_pos()[0])}, {int(self.player.get_pos()[1])}", True, Colors.WHITE.value)
+        pos_text = pygame.font.Font(None, 30).render(f"Posición: {int(self.player.pos[0])}, {int(self.player.pos[1])}", True, Colors.WHITE.value)
         self.screen_manager.screen.blit(pos_text, (10, 10))
 
         self.player.draw(self.screen_manager.screen)
 
     def update_houses(self):
-        self.screen_manager.api.get_houses(self.player.get_pos())
+        houses_data = self.screen_manager.api.get_houses(self.player.pos)
+        self.houses = [ House(house_info, self.screen_manager.screen, self.player.pos) for house_info in houses_data ]
