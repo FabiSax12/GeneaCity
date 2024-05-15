f17f828fd623217412edc1e2b354d44f8834b535
Fabián Vargas <fabidev18@gmail.com>
2024-05-15 17:45:21 -0600

    Refactor Sprite class for smoother animation and getters/setters implementation

--- src/visuals/sprite.py
+++ src/visuals/sprite.py
@@ -3,35 +3,47 @@ import pygame
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, position: tuple[int, int]):
         super().__init__()
-        self._sheet = pygame.image.load("src/assets/spritesheet.png")
-        self._sheet.set_clip(pygame.Rect(0, 0, 88, 105))
-        self._image = self._sheet.subsurface(self._sheet.get_clip())
-        self._image = pygame.transform.scale(self._image, (44, 52.5))
-        self._rect = self._image.get_rect()
-        self._rect.topleft = position
-        self._frame = 0
-        self._direction = "down"
-        self._animation = {
+        self.__sheet = pygame.image.load("src/assets/spritesheet.png")
+        self.__sheet.set_clip(pygame.Rect(0, 0, 88, 105))
+        self.__image = self.__sheet.subsurface(self.__sheet.get_clip())
+        self.__image = pygame.transform.scale(self.__image, (44, 52.5))
+        self.__rect = self.__image.get_rect()
+        self.__rect.topleft = position
+        self.__frame = 0
+        self.__direction = "down"
+        self.__animation = {
             "down": [(0, 0), (88, 0), (176, 0), (264, 0)],
             "up": [(0, 105), (88, 105), (176, 105), (264, 105)],
             "left": [(0, 210), (88, 210), (176, 210), (264, 210)],
             "right": [(0, 315), (88, 315), (176, 315), (264, 315)]
         }
 
-    def set_direction(self, direction: str):
-        self._direction = direction
-
     def _get_frame(self, frame_set):
-        self._frame += 0.05
-        if int(self._frame) > (len(frame_set) - 1):
-            self._frame = 0
-        return frame_set[int(self._frame)]
+        self.__frame += 0.05
+        if int(self.__frame) > (len(frame_set) - 1):
+            self.__frame = 0
+        return frame_set[int(self.__frame)]
     
     def _clip(self):
-        frame_coords = self._get_frame(self._animation[self._direction])
-        self._sheet.set_clip(pygame.Rect(*frame_coords, 88, 105))
+        frame_coords = self._get_frame(self.__animation[self.__direction])
+        self.__sheet.set_clip(pygame.Rect(*frame_coords, 88, 105))
 
     def update(self):
         self._clip()
-        self._image = self._sheet.subsurface(self._sheet.get_clip())
-        self._image = pygame.transform.scale(self._image, (44, 52.5))
+        self.__image = self.__sheet.subsurface(self.__sheet.get_clip())
+        self.__image = pygame.transform.scale(self.__image, (44, 52.5))
+
+    # Properties
+
+    @property
+    def image(self) -> pygame.Surface:
+        return self.__image
+
+    @property
+    def direction(self) -> str:
+        return self.__direction
+    
+    @direction.setter
+    def direction(self, value: str):
+        if value in self.__animation.keys() and value != self.__direction:
+            self.__direction = value
