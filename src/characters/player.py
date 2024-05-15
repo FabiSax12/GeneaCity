ca50b967d9b535241399ea15556233bf4d6b10e3
Fabián Vargas <fabidev18@gmail.com>
2024-05-15 17:59:03 -0600

    Encapsulate player attributes and update sprite handling

--- src/characters/player.py
+++ src/characters/player.py
@@ -3,12 +3,11 @@ from characters.person import Person
 from visuals.sprite import Sprite
 
 class Player(Person):
-    def __init__(self, person_info: dict, screen: pygame.Surface):
+    def __init__(self, person_info: dict):
         super().__init__(person_info)
-        self.screen = screen
-        self._score = 0
-        self.position = pygame.Vector2(person_info["house"]["x"], person_info["house"]["y"])
-        self.sprite = Sprite(self.position)
+        self.__score = 0
+        self.__position = pygame.Vector2(person_info["house"]["x"], person_info["house"]["y"])
+        self.__sprite = Sprite(self.__position)
 
     def draw(self, map_surface: pygame.Surface):
         """Draw the player on the map.
@@ -17,67 +16,93 @@ class Player(Person):
             map_surface (pygame.Surface): map surface
         """
         map_surface.blit(
-            self.sprite._image, 
+            self.__sprite.image, 
             (
-                map_surface.get_width() // 2 - self.sprite._image.get_width() // 2, 
-                map_surface.get_height() // 2 - self.sprite._image.get_height() // 2
+                map_surface.get_width() // 2 - self.__sprite.image.get_width() // 2, 
+                map_surface.get_height() // 2 - self.__sprite.image.get_height() // 2
             )
         )
 
-    def  move(self, dx: int, dy: int):
+    def move(self, dx: int, dy: int):
         """Move the player.
 
         Args:
             dx (int): difference in x-axis
             dy (int): difference in y-axis
         """
-        direction = "right" if dx > 0 else "left" if dx < 0 else "down" if dy > 0 else "up" if dy < 0 else self.sprite._direction
-        self.sprite.set_direction(direction)
+        direction = "right" if dx > 0 else "left" if dx < 0 else "down" if dy > 0 else "up" if dy < 0 else self.__sprite._direction
+        self.__sprite.direction = direction
         
-        if self.position.x + dx < 0:
-            self.position.x = 0
-        elif self.position.x + dx > 100000 - self.sprite._image.get_width():
-            self.position.x = 100000 - self.sprite._image.get_width()
+        if self.__position.x + dx < 0:
+            self.__position.x = 0
+        elif self.__position.x + dx > 100000 - self.__sprite.image.get_width():
+            self.__position.x = 100000 - self.__sprite.image.get_width()
         else:
-            self.position.x += dx
-            self.sprite.update()
+            self.__position.x += dx
+            self.__sprite.update()
 
-        if self.position.y + dy < 0:
-            self.position.y = 0
-        elif self.position.y + dy > 100000 - self.sprite._image.get_height():
-            self.position.y = 100000 - self.sprite._image.get_height()
+        if self.__position.y + dy < 0:
+            self.__position.y = 0
+        elif self.__position.y + dy > 100000 - self.__sprite.image.get_height():
+            self.__position.y = 100000 - self.__sprite.image.get_height()
         else:
-            self.position.y += dy
-            self.sprite.update()
+            self.__position.y += dy
+            self.__sprite.update()
 
-    def get_pos(self) -> tuple[int, int]:
+    def increase_score(self, points: int):
+        """Increase the player's score.
+
+        Args:
+            points (int): points to increase
+        """
+        if points < 0:
+            raise ValueError("Points must be a positive integer.")
+        
+        self.__score += points
+    
+    def decrease_score(self, points: int):
+        """Decrease the player's score.
+
+        Args:
+            points (int): points to decrease
+        """
+        if points < 0:
+            raise ValueError("Points must be a positive integer.")
+        
+        if self.__score - points < 0:
+            self.__score = 0
+        else:
+            self.__score -= points
+
+    # Properties
+    @property
+    def pos(self) -> tuple[int, int]:
         """Get the position of the player.
 
         Returns:
             tuple[int, int]: Position of the player in the map surface (x, y)
         """
-        return self.position.x, self.position.y
+        return self.__position.x, self.__position.y
 
-    def get_score(self) -> int:
-        """Get the player's score.
+    @property
+    def score(self) -> int:
+        """Get the player's score."""
+        return self.__score
 
-        Returns:
-            
-        """
-        return self._score
-
-    def set_score(self, score: int):
+    @score.setter
+    def score(self, score: int):
         """Set the player's score.
 
         Args:
             score (int): player's score
         """
-        self._score = score
-        
-    def get_player_info(self) -> dict:
+        self.__score = score
+
+    @property    
+    def info(self) -> dict:
         """Get the player's information.
 
         Returns:
             dict: player's information
         """
-        return {"id": self.id, "name": self.name, "age": self.age, "score": self._score, "x": self.position.x, "y": self.position.y}
+        return {"id": self.id, "name": self.name, "age": self.age, "score": self.score, "x": self.__position.x, "y": self.__position.y}
