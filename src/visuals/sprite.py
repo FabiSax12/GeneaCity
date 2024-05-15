7300712391a12947488ca8af2bcbe19ab7e6efc2
Fabián Vargas <fabidev18@gmail.com>
2024-05-14 23:02:39 -0600

    Refactor Sprite class for smoother animation

--- src/visuals/sprite.py
+++ src/visuals/sprite.py
@@ -10,23 +10,23 @@ class Sprite(pygame.sprite.Sprite):
         self._rect = self._image.get_rect()
         self._rect.topleft = position
         self._frame = 0
+        self._direction = "down"
         self._animation = {
             "down": [(0, 0), (88, 0), (176, 0), (264, 0)],
             "up": [(0, 105), (88, 105), (176, 105), (264, 105)],
             "left": [(0, 210), (88, 210), (176, 210), (264, 210)],
             "right": [(0, 315), (88, 315), (176, 315), (264, 315)]
         }
-        self._direction = "down"
 
     def set_direction(self, direction: str):
         self._direction = direction
 
     def _get_frame(self, frame_set):
-        self._frame += 1
-        if self._frame > (len(frame_set) - 1):
+        self._frame += 0.05
+        if int(self._frame) > (len(frame_set) - 1):
             self._frame = 0
-        return frame_set[self._frame]
-
+        return frame_set[int(self._frame)]
+    
     def _clip(self):
         frame_coords = self._get_frame(self._animation[self._direction])
         self._sheet.set_clip(pygame.Rect(*frame_coords, 88, 105))
