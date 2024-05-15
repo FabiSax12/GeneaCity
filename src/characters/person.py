10bf2820b01ee9dce168a6713fc374b1bb83dd67
Fabián Vargas <fabidev18@gmail.com>
2024-05-15 17:54:54 -0600

    Update Person class to use private attributes and add properties for encapsulation

--- src/characters/person.py
+++ src/characters/person.py
@@ -1,16 +1,59 @@
 class Person():
     def __init__(self, person_info: dict):
-        self.id = person_info["id"]
-        self.name = person_info["name"]
-        self.age = person_info["age"]
-        self.house = person_info["house"]
-        self.is_married = False
+        self.__id = person_info["id"]
+        self.__name = person_info["name"]
+        self.__gender = person_info["gender"]
+        self.__age = person_info["age"]
+        self.__marital_status = person_info["marital_status"]
+        self.__alive = person_info["alive"]
+        self.__father = person_info["father"]
+        self.__mother = person_info["mother"]
+        self.__house = person_info["house"]
 
     def __str__(self):
-        return f"{self.name} is {self.age} years old."
+        return f"{self.__name} is {self.__age} years old."
     
     def birthday(self):
-        self.age += 1
+        self.__age += 1
 
-    def get_info(self):
-        return {"id": self.id, "name": self.name, "age": self.age}
+    # Properties
+
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def gender(self):
+        return self.__gender
+    
+    @property
+    def age(self):
+        return self.__age
+    
+    @property
+    def marital_status(self):
+        return self.__marital_status
+    
+    @property
+    def alive(self):
+        return self.__alive
+    
+    @property
+    def father(self):
+        return self.__father
+    
+    @property
+    def mother(self):
+        return self.__mother
+    
+    @property
+    def house(self):
+        return self.__house
+    
+    @house.setter
+    def house(self, new_house):
+        self.__house = new_house
