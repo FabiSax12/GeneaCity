33f0d0f3f8eee52cb7625f9bc09b73f6aca24641
Fabián Vargas <fabidev18@gmail.com>
2024-05-17 00:13:17 -0600

    Update Sprite class to dynamically load spritesheet based on gender

--- src/visuals/sprite.py
+++ src/visuals/sprite.py
@@ -1,37 +1,64 @@
 import pygame
+from typing import Literal
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, position: tuple[int, int]):
+    def __init__(self, position: tuple[int, int], gender: Literal["Male", "Female"]):
         super().__init__()
-        self.__sheet = pygame.image.load("src/assets/spritesheet.png")
-        self.__sheet.set_clip(pygame.Rect(0, 0, 88, 105))
+        self.__sheet = pygame.image.load(f"src/assets/spritesheet{gender}4.png")
+        self.__sprite_width = self.__sheet.get_width() // 4
+        self.__sprite_height = self.__sheet.get_height() // 4
+
+        self.__sheet.set_clip(pygame.Rect(0, 0, self.__sprite_width, self.__sprite_height))
+
         self.__image = self.__sheet.subsurface(self.__sheet.get_clip())
-        self.__image = pygame.transform.scale(self.__image, (44, 52.5))
+        self.__image = pygame.transform.scale(self.__image, (self.__sprite_width, self.__sprite_height))
+
         self.__rect = self.__image.get_rect()
         self.__rect.topleft = position
+
         self.__frame = 0
         self.__direction = "down"
         self.__animation = {
-            "down": [(0, 0), (88, 0), (176, 0), (264, 0)],
-            "up": [(0, 105), (88, 105), (176, 105), (264, 105)],
-            "left": [(0, 210), (88, 210), (176, 210), (264, 210)],
-            "right": [(0, 315), (88, 315), (176, 315), (264, 315)]
+            "down": [
+                (0, 0), 
+                (self.__sprite_width, 0), 
+                (self.__sprite_width * 2, 0), 
+                (self.__sprite_width * 3, 0)
+            ],
+            "left": [
+                (0, self.__sprite_height),
+                (self.__sprite_width, self.__sprite_height),
+                (self.__sprite_width * 2, self.__sprite_height),
+                (self.__sprite_width * 3, self.__sprite_height)
+            ],
+            "right": [
+                (0, self.__sprite_height * 2),
+                (self.__sprite_width, self.__sprite_height * 2),
+                (self.__sprite_width * 2, self.__sprite_height * 2),
+                (self.__sprite_width * 3, self.__sprite_height * 2)
+            ],
+            "up": [
+                (0, self.__sprite_height * 3),
+                (self.__sprite_width, self.__sprite_height * 3),
+                (self.__sprite_width * 2, self.__sprite_height * 3),
+                (self.__sprite_width * 3, self.__sprite_height * 3)
+            ]
         }
 
     def _get_frame(self, frame_set):
-        self.__frame += 0.05
+        self.__frame += 0.04
         if int(self.__frame) > (len(frame_set) - 1):
             self.__frame = 0
         return frame_set[int(self.__frame)]
     
     def _clip(self):
         frame_coords = self._get_frame(self.__animation[self.__direction])
-        self.__sheet.set_clip(pygame.Rect(*frame_coords, 88, 105))
+        self.__sheet.set_clip(pygame.Rect(*frame_coords, self.__sprite_width, self.__sprite_height))
 
     def update(self):
         self._clip()
         self.__image = self.__sheet.subsurface(self.__sheet.get_clip())
-        self.__image = pygame.transform.scale(self.__image, (44, 52.5))
+        self.__image = pygame.transform.scale(self.__image, (self.__sprite_width, self.__sprite_height))
 
     # Properties
 
